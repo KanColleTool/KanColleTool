ede3fe6b0ddef3c1fcf86d4b4ef4ed099ac14584
uppfinnarn <uppfinnarn@gmail.com>
2014-01-16 15:43:55 +0100

    Macros go in KCUtil.h

--- tool/KCMainWindow.cpp
+++ tool/KCMainWindow.cpp
@@ -215,12 +215,6 @@ void KCMainWindow::updateFleetsPage()
 	ui->fleetsPage->setUpdatesEnabled(true);
 }
 
-#define TABLE_SET_ITEM(_table, _row, _col, _value) \
-	{\
-		QTableWidgetItem *item = new QTableWidgetItem(); \
-		item->setData(Qt::EditRole, _value); \
-		_table->setItem(_row, _col, item); \
-	}
 void KCMainWindow::updateShipsPage()
 {
 	ui->shipsPage->setUpdatesEnabled(false);
@@ -231,16 +225,6 @@ void KCMainWindow::updateShipsPage()
 	int row = 0;
 	foreach(KCShip *ship, client->ships)
 	{
-		/*ui->shipsTable->setItem(row, 0, new QTableWidgetItem(QString::number(ship->level)));
-		ui->shipsTable->setItem(row, 1, new QTableWidgetItem(QString::number(ship->maxHp)));
-		ui->shipsTable->setItem(row, 2, new QTableWidgetItem(QString::number(ship->firepower.cur)));
-		ui->shipsTable->setItem(row, 3, new QTableWidgetItem(QString::number(ship->torpedo.cur)));
-		ui->shipsTable->setItem(row, 4, new QTableWidgetItem(QString::number(ship->evasion.cur)));
-		ui->shipsTable->setItem(row, 5, new QTableWidgetItem(QString::number(ship->antiair.cur)));
-		ui->shipsTable->setItem(row, 6, new QTableWidgetItem(QString::number(ship->antisub.cur)));
-		ui->shipsTable->setItem(row, 7, new QTableWidgetItem(QString::number(ship->speed)));
-		ui->shipsTable->setItem(row, 8, new QTableWidgetItem(QString("%1 (%2)").arg(ship->name, ship->reading)));*/
-		
 		TABLE_SET_ITEM(ui->shipsTable, row, 0, ship->level);
 		TABLE_SET_ITEM(ui->shipsTable, row, 1, ship->maxHp);
 		TABLE_SET_ITEM(ui->shipsTable, row, 2, ship->firepower.cur);
--- tool/KCUtil.h
+++ tool/KCUtil.h
@@ -8,4 +8,11 @@ QTime delta(const QDateTime &d1, const QDateTime &d2 = QDateTime::currentDateTim
 	return QTime::fromMSecsSinceStartOfDay(msecs);
 }
 
+#define TABLE_SET_ITEM(_table, _row, _col, _value) \
+	{\
+		QTableWidgetItem *item = new QTableWidgetItem(); \
+		item->setData(Qt::EditRole, _value); \
+		_table->setItem(_row, _col, item); \
+	}
+
 #endif
