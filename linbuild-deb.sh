83536558c2c81685dc835588f81d5f59eebd0230
Johannes Ekberg <uppfinnarn@gmail.com>
2014-01-19 23:54:55 +0100

    Improved Linux Build Scripts

--- linbuild-deb.sh
+++ linbuild-deb.sh
@@ -3,19 +3,30 @@
 # This both puts the sources in the right place and gives us $VERSION
 source linbuild.sh
 
-# Make .orig tarballs, since debian's helper scripts really like that name
+# Set up a directory structure for the debian build
 cd dist
-cp kancolletool-${VERSION}.tar.gz kancolletool_${VERSION}.orig.tar.gz
-cp kancolletool-viewer-${VERSION}.tar.gz kancolletool-viewer_${VERSION}.orig.tar.gz
+mkdir debian
+cp src/kancolletool-${VERSION}.tar.gz debian/kancolletool_${VERSION}.orig.tar.gz
+cp src/kancolletool-viewer-${VERSION}.tar.gz debian/kancolletool-viewer_${VERSION}.orig.tar.gz
+
+cd debian
+tar -xf kancolletool_${VERSION}.orig.tar.gz
+tar -xf kancolletool-viewer_${VERSION}.orig.tar.gz
 
 # Debianize and package the Tool
+# Note that we're not signing these packages, simply because signing raises
+# annoying errors on every machines except the one my keys are on
 cd kancolletool-${VERSION}/
-cp -R ../../targets/debian/kancolletool/debian .
+cp -R ../../../targets/debian/kancolletool/debian .
 hash debuild >/dev/null 2>&1 && debuild -us -uc || dpkg-buildpackage
 cd ..
 
 # Debianize and package the Viewer
+# See above about the explicit lack of signing
 cd kancolletool-viewer-${VERSION}/
-cp -R ../../targets/debian/kancolletool-viewer/debian .
+cp -R ../../../targets/debian/kancolletool-viewer/debian .
 hash debuild >/dev/null 2>&1 && debuild -us -uc || dpkg-buildpackage
+cd ..
+
+# Go back to the root folder
 cd ..
--- linbuild.sh
+++ linbuild.sh
@@ -4,18 +4,18 @@ VERSION=$(git describe --tags | grep -Po 'v\K([\w\.]+)')
 
 # Remove old dist
 rm -rf dist
-mkdir dist
+mkdir -p dist/src
 
 # Prepare tool sources
-mkdir dist/kancolletool-${VERSION}
-cp -R tool/*.{pro,cpp,h,ui,qrc,png} dist/kancolletool-${VERSION}/
+mkdir dist/src/kancolletool-${VERSION}
+cp -R tool/*.{pro,cpp,h,ui,qrc,png} dist/src/kancolletool-${VERSION}/
 
 # Prepare viewer sources
-mkdir dist/kancolletool-viewer-${VERSION}
-cp -R viewer/*.{pro,cpp,h,qrc,png,html,js} viewer/proxy-plugins/ dist/kancolletool-viewer-${VERSION}/
+mkdir dist/src/kancolletool-viewer-${VERSION}
+cp -R viewer/*.{pro,cpp,h,qrc,png,html,js} viewer/proxy-plugins/ dist/src/kancolletool-viewer-${VERSION}/
 
 # Tarball it all up
-cd dist
+cd dist/src
 tar -czf kancolletool-${VERSION}.tar.gz kancolletool-${VERSION}
 tar -czf kancolletool-viewer-${VERSION}.tar.gz kancolletool-viewer-${VERSION}
-cd ..
+cd ../..
