993c7bc34d0cd586663508ab05072d1062b8b418
Johannes Ekberg <uppfinnarn@gmail.com>
2014-01-19 21:50:11 +0100

    And we have debs
    Fixes #5

--- linbuild-deb.sh
+++ linbuild-deb.sh
@@ -3,10 +3,19 @@
 # This both puts the sources in the right place and gives us $VERSION
 source linbuild.sh
 
-# Debianize and package the Tool
+# Make .orig tarballs, since debian's helper scripts really like that name
 cd dist
 cp kancolletool-${VERSION}.tar.gz kancolletool_${VERSION}.orig.tar.gz
+cp kancolletool-viewer-${VERSION}.tar.gz kancolletool-viewer_${VERSION}.orig.tar.gz
 
+# Debianize and package the Tool
 cd kancolletool-${VERSION}/
 cp -R ../../targets/debian/kancolletool/debian .
 dpkg-buildpackage
+cd ..
+
+# Debianize and package the Viewer
+cd kancolletool-viewer-${VERSION}/
+cp -R ../../targets/debian/kancolletool-viewer/debian .
+dpkg-buildpackage
+cd ..
--- linbuild.sh
+++ linbuild.sh
@@ -3,16 +3,16 @@
 VERSION=$(git describe --tags | grep -Po 'v\K([\w\.]+)')
 
 # Remove old dist
-rm -rf dist 2>/dev/null
+rm -rf dist
 mkdir dist
 
 # Prepare tool sources
 mkdir dist/kancolletool-${VERSION}
-cp -R tool/*.{pro,c,cpp,h,ui,qrc,png} dist/kancolletool-${VERSION}/ 2>/dev/null
+cp -R tool/*.{pro,cpp,h,ui,qrc,png} dist/kancolletool-${VERSION}/
 
 # Prepare viewer sources
 mkdir dist/kancolletool-viewer-${VERSION}
-cp -R viewer/*.{pro,c,cpp,h,ui,qrc,png} dist/kancolletool-viewer-${VERSION}/ 2>/dev/null
+cp -R viewer/*.{pro,cpp,h,qrc,png,html,js} viewer/proxy-plugins/ dist/kancolletool-viewer-${VERSION}/
 
 # Tarball it all up
 cd dist
