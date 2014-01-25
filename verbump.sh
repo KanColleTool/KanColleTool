502f18b1aa55c9728293a8737ee39a97991f2daf
uppfinnarn <uppfinnarn@gmail.com>
2014-01-25 22:58:20 +0100

    THERE

--- verbump.sh
+++ verbump.sh
@@ -11,7 +11,7 @@ VERSION_RC="${1}, ${2}, ${3}, 0"
 echo $VERSION > VERSION
 
 sed -i '' -e "s/#define KCT_VERSION .*/#define KCT_VERSION \"$VERSION\"/" {tool,viewer}/version.h
-sed -i '' -e "s/#define MyAppVersion .*/#define MyAppVersion $VERSION/" targets/windows/KanColleTool.iss
+sed -i '' -e "s/#define MyAppVersion .*/#define MyAppVersion \"$VERSION\"/" targets/windows/KanColleTool.iss
 sed -i '' -e "s/VERSION = .*/VERSION = $VERSION/" {tool,viewer}/*.pro
 sed -n -i '' -e '/CFBundleShortVersionString/{p;n;s/>.*</>'$VERSION'</;};p' macviewer/KCTViewer/KCTViewer/KCTViewer-Info.plist
 sed -i '' -e "s/\( *\)FILEVERSION\( *\).*/\1FILEVERSION\2${VERSION_RC}/" {tool,viewer}/*.rc
