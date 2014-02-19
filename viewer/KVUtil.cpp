a4d6f66ed63083f1f5bd3a8e03b3f9540c64b62a
Sauyon Lee <sauyonl@sauyon.com>
2014-02-19 15:02:55 +0900

    use QRegularExpression instead of QRegExp

--- viewer/KVUtil.cpp
+++ viewer/KVUtil.cpp
@@ -1,17 +1,20 @@
 #include "KVUtil.h"
-#include <QRegExp>
+#include <QRegularExpression>
 #include <QChar>
 
-// 
+//
 // Taken from http://stackoverflow.com/a/7047000
-// TODO: Change the QRegExp (which is bad) to a QRegularExpression (good).
-// 
+//
 QString unescape(QString str)
 {
-	QRegExp rx("(\\\\u[0-9a-fA-F]{4})");
-	int pos = 0;
-	while ((pos = rx.indexIn(str, pos)) != -1)
-		str.replace(pos++, 6, QChar(rx.cap(1).right(4).toUShort(0, 16)));
-	
+	QRegularExpression re("(\\\\u[0-9a-fA-F]{4})");
+	QRegularExpressionMatchIterator it = re.globalMatch(str);
+	while (it.hasNext())
+	{
+		QRegularExpressionMatch match = it.next();
+		str.replace(match.capturedStart(), match.capturedLength(),
+		            QChar(match.captured(1).right(4).toUShort(0, 16)));
+	}
+
 	return str;
 }
