fe036791cfae15868188b53beb1317a9b055306d
uppfinnarn <uppfinnarn@gmail.com>
2014-02-20 09:25:30 +0100

    A bit more consistent coding style

--- viewer/KVNetworkAccessManager.cpp
+++ viewer/KVNetworkAccessManager.cpp
@@ -5,11 +5,12 @@
 #include "KVNetworkReply.h"
 
 KVNetworkAccessManager::KVNetworkAccessManager(QObject *parent) :
-	QNetworkAccessManager(parent) {}
+	QNetworkAccessManager(parent)
+{
+	
+}
 
-QNetworkReply *KVNetworkAccessManager::createRequest(Operation op,
-                                                     const QNetworkRequest &req,
-                                                     QIODevice *outgoingData)
+QNetworkReply *KVNetworkAccessManager::createRequest(Operation op, const QNetworkRequest &req, QIODevice *outgoingData)
 {
 	QNetworkRequest request = req;
 	request.setRawHeader("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.107 Safari/537.36");
@@ -22,7 +23,9 @@ QNetworkReply *KVNetworkAccessManager::createRequest(Operation op,
 			QNetworkReply *r = QNetworkAccessManager::createRequest(op, request, outgoingData);
 			KVNetworkReply *reply = new KVNetworkReply(r->parent(), r, this);
 			return reply;
-		} else if(op == QNetworkAccessManager::GetOperation) {
+		}
+		else if(op == QNetworkAccessManager::GetOperation)
+		{
 			qDebug() << "GET" << request.url().path();
 		}
 	}
--- viewer/KVNetworkAccessManager.h
+++ viewer/KVNetworkAccessManager.h
@@ -7,13 +7,12 @@
 class KVNetworkAccessManager : public QNetworkAccessManager
 {
 	Q_OBJECT
+	
 public:
-	explicit KVNetworkAccessManager( QObject *parent = 0 );
+	explicit KVNetworkAccessManager(QObject *parent = 0);
 
 protected:
-	QNetworkReply *createRequest( Operation op,
-	                              const QNetworkRequest &req,
-	                              QIODevice *outgoingData );
+	QNetworkReply *createRequest(Operation op, const QNetworkRequest &req, QIODevice *outgoingData);
 };
 
 #endif // KVNETWORKACCESSMANAGER_H
