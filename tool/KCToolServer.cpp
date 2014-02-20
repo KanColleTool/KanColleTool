b3005af2e24bd443c3bc8547b9a52f08d471d053
Sauyon Lee <sauyonl@sauyon.com>
2014-02-19 22:33:09 -0800

    This is a server that only accepts data; a response is not really necessary

--- tool/KCToolServer.cpp
+++ tool/KCToolServer.cpp
@@ -10,7 +10,7 @@ KCToolServer::KCToolServer(KCClient *client, QObject *parent):
 
 KCToolServer::~KCToolServer()
 {
-	
+
 }
 
 void KCToolServer::handleRequest(QTcpSocket *socket)
@@ -20,18 +20,15 @@ void KCToolServer::handleRequest(QTcpSocket *socket)
 	QString path = socket->property("path").toString();
 	QByteArray content = socket->property("buffer").toByteArray();
 	QVariantMap headers = socket->property("headers").toMap();
-	
-	// Hold response data
-	int resCode(200);
-	QString resMsg("OK");
-	QString resType("text/json");
-	QByteArray resBody = "{ \"success\": true }";
-	
+
+	// Write out a reply to the client
+	reply(socket);
+
 	// Handle POSTs
 	if(method == "POST")
 	{
 		QVariant data = client->dataFromRawResponse(content);
-		
+
 		if(path == "/kcsapi/api_get_master/ship")
 			client->_processMasterShipsData(data);
 		else if(path == "/kcsapi/api_get_member/ship")
@@ -44,22 +41,15 @@ void KCToolServer::handleRequest(QTcpSocket *socket)
 			client->_processPlayerConstructionsData(data);
 		else
 		{
-			//qDebug() << "Dunno what to do about" << path;
-			resCode = 404;
-			resMsg = "Not Found";
+			qDebug() << "Unknown api path:" << path;
 		}
 	}
 	// I might add other methods later (if I find a use), but for now, refuse them
 	else
 	{
 		qDebug() << "Invalid Method:" << method << "(" << path << ")";
-		resCode = 405;
-		resMsg = "Method Not Allowed";
 	}
-	
-	// Write out a reply to the client
-	reply(socket, resCode, resMsg, resType, (resBody.size() > 0 ? resBody : resMsg.toUtf8()));
-	
+
 	// If it's not a Connection: close request, reset the state properties
 	if(headers.value("Connection").toString() != "close")
 	{
@@ -75,14 +65,9 @@ void KCToolServer::handleRequest(QTcpSocket *socket)
 		socket->close();
 }
 
-void KCToolServer::reply(QTcpSocket *socket, int code, QString message, QString contentType, QByteArray body)
+void KCToolServer::reply(QTcpSocket *socket, int code, QString message)
 {
-	socket->write(QString("HTTP/1.1 %1 %2\r\n").arg(QString::number(code), message).toUtf8());
-	socket->write(QString("Date: %1\r\n").arg(QDateTime::currentDateTimeUtc().toString("ddd, d MMM yyyy hh:mm:ss")).toUtf8());
-	socket->write(QString("Content-Length: %1\r\n").arg(QString::number(body.size())).toUtf8());
-	socket->write(QString("Content-Type: %1\r\n").arg(contentType).toUtf8());
-	socket->write(QString("\r\n").toUtf8());
-	socket->write(body);
+	socket->write("HTTP/1.1 204 No Content\r\n\r\n");
 }
 
 void KCToolServer::onNewConnection()
@@ -97,7 +82,7 @@ void KCToolServer::onNewConnection()
 void KCToolServer::onSocketReadyRead()
 {
 	QTcpSocket *socket = qobject_cast<QTcpSocket*>(QObject::sender());
-	
+
 	// Parse the first line
 	if(!socket->property("firstLineRead").toBool())
 	{
@@ -110,49 +95,49 @@ void KCToolServer::onSocketReadyRead()
 		socket->setProperty("path", path);
 		socket->setProperty("firstLineRead", true);
 	}
-	
+
 	// Parse Headers!
 	int contentLength = -1;
 	if(!socket->property("headerRead").toBool())
 	{
 		QVariantMap headers(socket->property("headers").toMap());
-		
+
 		while(socket->canReadLine())
 		{
 			QString line = QString(socket->readLine()).trimmed();
-			
+
 			// The header section is terminated by an empty line
 			if(line == "")
 			{
 				socket->setProperty("headerRead", true);
 				break;
 			}
-			
+
 			// Split it up
 			int sepPos(line.indexOf(":"));
 			QString key(line.left(sepPos).trimmed());
 			QString val(line.mid(sepPos+1).trimmed());
 			headers.insertMulti(key, val);
-			
+
 			// Check for the Content-Length header
 			if(key == "Content-Length")
 				contentLength = val.toInt();
 		}
-		
+
 		socket->setProperty("headers", headers);
 	}
-	
+
 	// Read the body into a buffer
 	if(socket->bytesAvailable())
 	{
 		QByteArray buffer(socket->property("buffer").toByteArray());
 		buffer.append(socket->readAll());
 		socket->setProperty("buffer", buffer);
-		
+
 		// If this is a later chunk, check the old Content-Length header
 		if(contentLength == -1)
 			contentLength = socket->property("headers").toMap().value("Content-Length").toInt();
-		
+
 		// If we have a Content-Length (toLong() fails with 0)
 		if(contentLength > 0 && buffer.size() >= contentLength)
 		{
