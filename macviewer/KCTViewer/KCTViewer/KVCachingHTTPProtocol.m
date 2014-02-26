525899a135962d3fc0f92ab685a61c01eaaf0f2e
uppfinnarn <uppfinnarn@gmail.com>
2014-02-26 12:45:23 +0100

    Eliminated unnecessary HEAD requests

--- macviewer/KCTViewer/KCTViewer/KVCachingHTTPProtocol.m
+++ macviewer/KCTViewer/KCTViewer/KVCachingHTTPProtocol.m
@@ -9,7 +9,14 @@
 #import "KVCachingHTTPProtocol.h"
 #import "NSString+KVHashes.h"
 
+static NSMutableSet *loadedURLs;
+
 @implementation KVCachingHTTPProtocol
+
++ (void)load
+{
+	loadedURLs = [[NSMutableSet alloc] init];
+}
 
 + (BOOL)canInitWithRequest:(NSURLRequest *)request
 {
@@ -33,14 +40,33 @@
 {
 	[[self class] setProperty:[NSNumber numberWithBool:YES] forKey:@"_handled" inRequest:(NSMutableURLRequest*)self.request];
 	
+	BOOL load = YES;
 	if((self.cacheEntry = [NSKeyedUnarchiver unarchiveObjectWithFile:[self cachePath]]) && self.cacheEntry.response && self.cacheEntry.data)
 	{
 		NSLog(@"   -- Cache Entry Exists");
-		[(NSMutableURLRequest *)self.request setValue:[[(NSHTTPURLResponse *)self.cacheEntry.response allHeaderFields] valueForKey:@"Date"] forHTTPHeaderField:@"If-Modified-Since"];
+		
+		// As a quick-and-dirty alternative to proper handling of the "Cache-Control" header, we just check cache validity
+		// based on if the URL has been loaded before during this session.
+		if(![loadedURLs containsObject:self.request.URL])
+		{
+			// If it hasn't been loaded before on this launch, set the "If-Modified-Since" header to make the remote server
+			// return a HTTP 304 and no data if the cached data is valid, otherwise the real data.
+			[(NSMutableURLRequest *)self.request setValue:[[(NSHTTPURLResponse *)self.cacheEntry.response allHeaderFields] valueForKey:@"Date"] forHTTPHeaderField:@"If-Modified-Since"];
+		}
+		else
+		{
+			// If it has been loaded before, just assume it's valid and send back the cached stuff. Also don't load it twice.
+			load = NO;
+			[self.client URLProtocol:self didReceiveResponse:self.cacheEntry.response cacheStoragePolicy:NSURLCacheStorageNotAllowed];
+			[self.client URLProtocol:self didLoadData:self.cacheEntry.data];
+			[self.client URLProtocolDidFinishLoading:self];
+		}
 	}
 	else self.cacheEntry = [[KVCacheEntry alloc] initWithRequest:self.request];
 	
-	self.connection = [NSURLConnection connectionWithRequest:self.request delegate:self];
+	// Only load if it hasn't already been loaded from the cache
+	if(load)
+		self.connection = [NSURLConnection connectionWithRequest:self.request delegate:self];
 }
 
 - (void)stopLoading
@@ -65,6 +91,7 @@
 	if([(NSHTTPURLResponse *)response statusCode] == 304)
 	{
 		NSLog(@"   -- Unmodified!");
+		[loadedURLs addObject:self.request.URL];
 		[self.client URLProtocol:self didReceiveResponse:self.cacheEntry.response cacheStoragePolicy:NSURLCacheStorageNotAllowed];
 		[self.client URLProtocol:self didLoadData:self.cacheEntry.data];
 		[self.client URLProtocolDidFinishLoading:self];
@@ -85,6 +112,7 @@
 - (void)connectionDidFinishLoading:(NSURLConnection *)connection
 {
 	[NSKeyedArchiver archiveRootObject:self.cacheEntry toFile:[self cachePath]];
+	[loadedURLs addObject:self.request.URL];
 	[self.client URLProtocolDidFinishLoading:self];
 	self.connection = nil;
 }
