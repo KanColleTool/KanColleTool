a1e1a561feb512cf9133fa0653c2b8ca8a5de9b1
uppfinnarn <uppfinnarn@gmail.com>
2014-02-26 13:29:36 +0100

    Unit Tests for the Translator

--- macviewer/KCTViewer/KCTViewerTests/KCTViewerTests.mm
+++ macviewer/KCTViewer/KCTViewerTests/KCTViewerTests.mm
@@ -7,6 +7,7 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "KVTranslator.h"
 
 @interface KCTViewerTests : XCTestCase
 
@@ -31,6 +32,27 @@
 	XCTAssertEqual(NSOrderedAscending, [@"0.8.1" compare:@"0.8.2" options:NSNumericSearch], @"0.8.1 >= 0.8.2");
 	XCTAssertEqual(NSOrderedSame, [@"0.8.2" compare:@"0.8.2" options:NSNumericSearch], @"0.8.2 != 0.8.2");
 	XCTAssertEqual(NSOrderedDescending, [@"0.8.3" compare:@"0.8.2" options:NSNumericSearch], @"0.8.3 <= 0.8.2");
+}
+
+- (void)testTranslation
+{
+	// Local translator instance - not a shared one, the last thing I need right now is singeltons
+	KVTranslator *translator = [[KVTranslator alloc] init];
+	
+	// Set up some test data - 那珂 (Naka) has a translation, まるゆ (Maruyu) doesn't
+	translator.tldata = @{ @"124853853": @"Naka", @"3440185848": [NSNull null] };
+	
+	// Try translating a translated string (Naka/那珂)
+	XCTAssertEqualObjects([translator translate:@"那珂"], @"Naka", @"那珂 doesn't translate to Naka!");
+	XCTAssertEqualObjects([translator translate:@"\\u90A3\\u73C2"], @"Naka", @"那珂 (escaped) doesn't translate to Naka!");
+	
+	// Try translating an unknown string (tesuto/テスト)
+	XCTAssertEqualObjects([translator translate:@"テスト"], @"テスト", @"Unknown strings aren't untouched!");
+	XCTAssertEqualObjects([translator translate:@"\\u30C6\\u30B9\\u30C8"], @"\\u30C6\\u30B9\\u30C8", @"Unknown Escaped strings aren't untouched!");
+	
+	// Try translating an untranslated string (Maruyu/まるゆ)
+	XCTAssertEqualObjects([translator translate:@"まるゆ"], @"まるゆ", @"Untranslated strings aren't untouched!");
+	XCTAssertEqualObjects([translator translate:@"\\u307E\\u308B\\u3086"], @"\\u307E\\u308B\\u3086", @"Untranslated Escaped strings aren't untouched!");
 }
 
 @end
