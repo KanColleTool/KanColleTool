195ebca83c08a9cf506ba3b3c0f99ad55ec54605
Sauyon Lee <sauyonl@sauyon.com>
2014-03-01 20:25:12 -0800

    Fix .deb target creation

--- linbuild-deb.sh
+++ linbuild-deb.sh
@@ -6,26 +6,30 @@ VERSION=$(<VERSION)
 cd dist
 rm -rf debian > /dev/null
 mkdir debian
-cp src/kancolletool-${VERSION}.tar.gz debian/kancolletool_${VERSION}.orig.tar.gz
-cp src/kancolletool-viewer-${VERSION}.tar.gz debian/kancolletool-viewer_${VERSION}.orig.tar.gz
 
 cd debian
-tar -xf kancolletool_${VERSION}.orig.tar.gz
-tar -xf kancolletool-viewer_${VERSION}.orig.tar.gz
+mkdir kancolletool-${VERSION}
+cp -a ../../tool/*.{pro,cpp,h,ui,qrc,png} kancolletool-${VERSION}/
+mkdir kancolletool-viewer-${VERSION}
+cp -a ../../viewer/*.{pro,cpp,h,ui,qrc,png,html,js} kancolletool-viewer-${VERSION}/
 
 # Debianize and package the Tool
 # Note that we're not signing these packages, simply because signing raises
 # annoying errors on every machines except the one my keys are on
 cd kancolletool-${VERSION}/
-cp -Rp ../../../targets/debian/kancolletool/debian .
-hash debuild >/dev/null 2>&1 && debuild -i -us -uc || dpkg-buildpackage
+qmake
+cp -a ../../../targets/debian/kancolletool/debian .
+dh_make -s --createorig -y
+hash debuild >/dev/null 2>&1 && debuild -i -us -uc || dpkg-buildpackage -d -us -uc
 cd ..
 
 # Debianize and package the Viewer
 # See above about the explicit lack of signing
 cd kancolletool-viewer-${VERSION}/
-cp -Rp ../../../targets/debian/kancolletool-viewer/debian .
-hash debuild >/dev/null 2>&1 && debuild -i -us -uc || dpkg-buildpackage
+qmake
+cp -a ../../../targets/debian/kancolletool-viewer/debian .
+dh_make -s --createorig -y
+hash debuild >/dev/null 2>&1 && debuild -i -us -uc || dpkg-buildpackage -d -us -uc
 cd ..
 
 # Go back to the root folder
