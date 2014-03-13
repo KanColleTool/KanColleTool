12d5e9d341623a817f34c5a9de36ee7a03fe44ed
uppfinnarn <uppfinnarn@gmail.com>
2014-03-13 19:36:23 +0100

    Fixed version bump script

--- verbump.sh
+++ verbump.sh
@@ -11,9 +11,9 @@ VERSION_RC="${1}, ${2}, ${3}, 0"
 git tag "v$1.$2.$3"
 echo $VERSION > VERSION
 
-sed -i '' -e "s/#define KCT_VERSION .*/#define KCT_VERSION \"$VERSION\"/" {tool,viewer}/version.h
+sed -i '' -e "s/#define KCT_VERSION .*/#define KCT_VERSION \"$VERSION\"/" tool/src/version.h viewer/version.h
 sed -i '' -e "s/#define MyAppVersion .*/#define MyAppVersion \"$VERSION\"/" targets/windows/KanColleTool.iss
-sed -i '' -e "s/VERSION = .*/VERSION = $VERSION/" {tool,viewer}/*.pro
-sed -n -i '' -e '/CFBundleShortVersionString/{p;n;s/>.*</>'$VERSION'</;};p' macviewer/KCTViewer/KCTViewer/KCTViewer-Info.plist
+sed -i '' -e "s/VERSION = .*/VERSION = $VERSION/" tool/*.pri viewer/*.pro
+sed -n -i '' -e '/CFBundleShortVersionString/{p;n;s/>.*</>'$VERSION'</;};p' macviewer/KCTViewer/KCTViewer-Info.plist
 sed -i '' -e "s/\( *\)FILEVERSION\( *\).*/\1FILEVERSION\2${VERSION_RC}/" {tool,viewer}/*.rc
 sed -i '' -e "s/\( *\)PRODUCTVERSION\( *\).*/\1PRODUCTVERSION\2${VERSION_RC}/" {tool,viewer}/*.rc
