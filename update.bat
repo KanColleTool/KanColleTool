6bdeff7747968c116adc119be53fe4a7dbd25c49
Johannes Ekberg <uppfinnarn@gmail.com>
2014-03-13 19:28:25 +0100

    Windows update script (bat syntax is bullshit)

---
+++ update.bat
@@ -0,0 +1,19 @@
+REM @echo off
+
+for /d %%f in (%LOCALAPPDATA%\GitHub\PortableGit_*) do (
+	PATH=%%f\bin;%PATH%
+	break
+)
+
+call:update_repo tool
+call:update_repo viewer
+
+pause
+
+:update_repo
+if exist %~1 (
+	cd %~1 && git pull --rebase && cd ..
+) else (
+	git clone "git://github.com/KanColleTool/kct-%~1.git" %~1
+)
+goto:eof
