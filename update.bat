ca9859527a316d946ccde85ad72a2ea4e160ba7e
Johannes Ekberg <uppfinnarn@gmail.com>
2014-04-01 18:00:51 +0200

    Updated update.bat to be a bit less ugly

--- update.bat
+++ update.bat
@@ -1,25 +1,9 @@
 @echo off
 
-for /d %%f in (%LOCALAPPDATA%\GitHub\PortableGit_*) do (
-	PATH=%%f\bin;%%PATH%%
-	break
-)
+for /d %%f in (%LOCALAPPDATA%\GitHub\PortableGit_*) do ( set git=%%f\bin\git.exe )
 
-echo "--> Updating main repo..."
-git stash
-git pull --rebase
-git stash pop
+%git% stash
+%git% pull --rebase
+%git% stash pop
 
-call:updateRepo tool
-call:updateRepo viewer
-
-:updateRepo
-if exist %~1 (
-	echo."--> Updating %~1..."
-	cd %~1
-	git pull --rebase
-	cd ..
-) else (
-	git clone "git://github.com/KanColleTool/kct-%~1.git" %~1
-)
-goto:eof
+%git% submodule foreach --recursive "git pull --rebase || git pull --rebase origin master"
