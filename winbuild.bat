a5806091bef3fd642a1385deb0d45269f4335c08
Johannes Ekberg <uppfinnarn@gmail.com>
2014-04-02 11:15:09 +0200

    Properly clean up in the Windows build script
    These changes to build.bat pretty much halve the Windows package size...
    I wonder if we can go even lower!

--- winbuild.bat
+++ winbuild.bat
@@ -7,6 +7,7 @@ set PATH=%PATH%;%QTBIN%
 set PATH=%PATH%;%QTDIR%\Tools\mingw48_32\bin
 set PATH=%PATH%;C:\Qt\Tools\mingw48_32\bin
 
+:build
 :: Delete old dist dir; the last thing we need is partial builds
 rmdir /s /q dist
 mkdir dist
@@ -32,6 +33,7 @@ rmdir /s /q debug
 rmdir /s /q release
 cd ..
 
+:package
 :: Copy MinGW32 DLLs that Qt doesn't catch automatically
 xcopy %QTDIR%\mingw48_32\bin\libgcc_s_dw2-1.dll dist\KanColleTool
 xcopy %QTDIR%\mingw48_32\bin\libstdc++-6.dll dist\KanColleTool
@@ -42,17 +44,22 @@ windeployqt --no-translations dist\KanColleTool\KCTViewer.exe
 windeployqt --no-translations dist\KanColleTool\KanColleTool.exe
 
 :: Delete things we don't need at all
+rmdir /s /q dist\KanColleTool\accessible
+rmdir /s /q dist\KanColleTool\iconengines
+rmdir /s /q dist\KanColleTool\imageformats
+rmdir /s /q dist\KanColleTool\mediaservice
 rmdir /s /q dist\KanColleTool\playlistformats
 rmdir /s /q dist\KanColleTool\printsupport
 rmdir /s /q dist\KanColleTool\qmltooling
-rmdir /s /q dist\KanColleTool\sqldrivers
 
 :: Delete debug versions of frameworks
-del /q dist\KanColleTool\accessible\*d.dll
 del /q dist\KanColleTool\bearer\*d.dll
-del /q dist\KanColleTool\iconengines\*d.dll
-del /q dist\KanColleTool\imageformats\*d.dll
-del /q dist\KanColleTool\mediaservice\*d.dll
 del /q dist\KanColleTool\platforms\*d.dll
+del /q dist\KanColleTool\sqldrivers\*d.dll
+
+:: Delete all database drivers but sqlite
+del /q dist\KanColleTool\sqldrivers\qsqlmysql*
+del /q dist\KanColleTool\sqldrivers\qsqlodbc*
+del /q dist\KanColleTool\sqldrivers\qsqlpsql*
 
 ::pause
