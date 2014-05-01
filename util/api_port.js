1133817e9efccd9c0822b2c801c32a8927ca6e81
Johannes Ekberg <uppfinnarn@gmail.com>
2014-05-01 20:40:21 +0200

    Update api_port.js

--- util/api_port.js
+++ util/api_port.js
@@ -3,12 +3,7 @@ function _(u) {
 	var loc = [1171, 1841, 2517, 3101, 4819, 5233, 6311, 7977, 8103, 9377, 1000]
 	var ret = 
 		(loc[10] + u % loc[10]).toString() +
-		(
-			(9.999999999E9 -
-				Math.floor(new Date().getTime() / loc[10]) -
-				u
-			) * loc[u % 10]
-		).toString() +
+		((9999999999 - Math.floor(new Date().getTime() / loc[10]) - u) * loc[u % 10]).toString() +
 		cN() + cN() + cN() + cN();
 	return ret;
 }
