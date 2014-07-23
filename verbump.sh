60e42facae14f2734dad7dbc4ce346b3bc9039c0
Johannes Ekberg <uppfinnarn@gmail.com>
2014-07-23 12:38:28 +0200

    Updated verbump.sh

--- verbump.sh
+++ verbump.sh
@@ -6,14 +6,11 @@ if [ $# -ne 3 ]; then
 fi
 
 VERSION="${1}.${2}.${3}"
-VERSION_RC="${1}, ${2}, ${3}, 0"
 
 echo $VERSION > VERSION
 
-sed -i '' -e "s/#define KCT_VERSION .*/#define KCT_VERSION \"$VERSION\"/" {tool,viewer}/src/version.h
+sed -i '' -e "s/set\(VERSION_MAJOR .*\)/set\(VERSION_MAJOR $1\)/" {tool,viewer}/src/CMakeLists.txt
+sed -i '' -e "s/set\(VERSION_MINOR .*\)/set\(VERSION_MINOR $2\)/" {tool,viewer}/src/CMakeLists.txt
+sed -i '' -e "s/set\(VERSION_PATCH .*\)/set\(VERSION_PATCH $3\)/" {tool,viewer}/src/CMakeLists.txt
 sed -i '' -e "s/#define MyAppVersion .*/#define MyAppVersion \"$VERSION\"/" targets/windows/KanColleTool.iss
-sed -i '' -e "s/VERSION = .*/VERSION = $VERSION/" tool/*.pri viewer/*.pro
 sed -n -i '' -e '/CFBundleShortVersionString/{p;n;s/>.*</>'$VERSION'</;};p' macviewer/KCTViewer/KCTViewer-Info.plist
-sed -i '' -e "s/\( *\)FILEVERSION\( *\).*/\1FILEVERSION\2${VERSION_RC}/" {tool,viewer}/*.rc
-sed -i '' -e "s/\( *\)PRODUCTVERSION\( *\).*/\1PRODUCTVERSION\2${VERSION_RC}/" {tool,viewer}/*.rc
-
