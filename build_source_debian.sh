f3549fef6bbed59bb3a4a148aaeb4252ec476e88
Johannes Ekberg <uppfinnarn@gmail.com>
2014-07-28 01:18:10 +0200

    Optionally do not actually build

--- build_source_debian.sh
+++ build_source_debian.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 set -e
 
+NO_DEBUILD=false
+
+for arg in "$@"; do
+	case $arg in
+		--no-debuild)
+			NO_DEBUILD=true
+	esac
+done
+
 VERSION=$(<VERSION)
 
 ./build_source_tgz.sh
 
 [[ -d dist/debian ]] && rm -rf dist/debian
 mkdir -p dist/debian && cd dist/debian
+
 cp ../kancolletool-$VERSION.tar.gz kancolletool_$VERSION.orig.tar.gz
 tar -xzf kancolletool_$VERSION.orig.tar.gz
 cd kancolletool-$VERSION
 
 cp -R ../../../targets/debian/* .
-debuild -S
+$NO_DEBUILD || debuild -S
