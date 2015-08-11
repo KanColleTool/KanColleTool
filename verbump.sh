128063db53a8e54d6f69c303286d174c1a2e8f4a
uppfinnarn <uppfinnarn@gmail.com>
2015-08-11 11:19:18 +0200

    0.9.5 bump

--- verbump.sh
+++ verbump.sh
@@ -9,8 +9,8 @@ VERSION="${1}.${2}.${3}"
 
 echo $VERSION > VERSION
 
-sed -i '' -e "s/set\(VERSION_MAJOR .*\)/set\(VERSION_MAJOR $1\)/" {.,tool/src,viewer/src}/CMakeLists.txt
-sed -i '' -e "s/set\(VERSION_MINOR .*\)/set\(VERSION_MINOR $2\)/" {.,tool/src,viewer/src}/CMakeLists.txt
-sed -i '' -e "s/set\(VERSION_PATCH .*\)/set\(VERSION_PATCH $3\)/" {.,tool/src,viewer/src}/CMakeLists.txt
+sed -i '' -e "s/set.VERSION_MAJOR .*/set\(VERSION_MAJOR $1\)/" {.,tool/src,viewer/src}/CMakeLists.txt
+sed -i '' -e "s/set.VERSION_MINOR .*/set\(VERSION_MINOR $2\)/" {.,tool/src,viewer/src}/CMakeLists.txt
+sed -i '' -e "s/set.VERSION_PATCH .*/set\(VERSION_PATCH $3\)/" {.,tool/src,viewer/src}/CMakeLists.txt
 sed -i '' -e "s/#define MyAppVersion .*/#define MyAppVersion \"$VERSION\"/" targets/windows/KanColleTool.iss
 sed -n -i '' -e '/CFBundleShortVersionString/{p;n;s/>.*</>'$VERSION'</;};p' macviewer/KCTViewer/KCTViewer-Info.plist
